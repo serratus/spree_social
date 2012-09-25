14d83793d92f62ab1d3acd1915de0ffc2ccd521a
Christoph Oberhofer <ch.oberhofer@gmail.com>
2012-09-25 00:18:22 -0400

    added ssl_required

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -1,6 +1,8 @@
 class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include Spree::Core::CurrentOrder
   include Spree::Core::ControllerHelpers
+  
+  ssl_required
 
   def self.provides_callback_for(*providers)
     providers.each do |provider|
